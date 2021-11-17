8cfe2bc0796ec97bca83d4cb7384b6556fbe658b
JrOrOneEquals1 <cdr5of8@gmail.com>
2021-11-17 14:29:04 -0700

    add looping code

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import datetime
+import time
 utc = datetime.timezone.utc
 timePassed = int((datetime.datetime.now().astimezone(utc) - datetime.datetime(2021, 11, 17, 18, 59, 0, tzinfo=utc)).total_seconds())
 datetimeList = [0, 0, 0, 0, 0, 0]
@@ -8,5 +9,22 @@ while timePassed >= 1:
     datetimeList[timesDone] = timePassed[1]
     timePassed = int(timePassed[0])
     timesDone += 1
+datetimeString = f"{datetimeList[5]}/{datetimeList[4]}/{datetimeList[3]}   {datetimeList[2]}:{datetimeList[1]}:{datetimeList[0]}"
 print(f"{str(datetime.datetime.now()).split('.')[0]} in Galactic Time is:")
-print("{datetimeList[5]}/{datetimeList[4]}/{datetimeList[3]}   {datetimeList[2]}:{datetimeList[1]}:{datetimeList[0]}")
+print(datetimeString)
+print('\n')
+
+if input('Loop? [y/n]\n>^< ').startswith('y'):
+    while True:
+        utc = datetime.timezone.utc
+        timePassed = int((datetime.datetime.now().astimezone(utc) - datetime.datetime(2021, 11, 17, 18, 59, 0, tzinfo=utc)).total_seconds())
+        datetimeList = [0, 0, 0, 0, 0, 0]
+        timesDone = 0
+        while timePassed >= 1:
+            timePassed = str(timePassed / 100).split('.')
+            datetimeList[timesDone] = timePassed[1]
+            timePassed = int(timePassed[0])
+            timesDone += 1
+        datetimeString = f"{datetimeList[5]}/{datetimeList[4]}/{datetimeList[3]}   {datetimeList[2]}:{datetimeList[1]}:{datetimeList[0]}"
+        print('\b'*len(list(datetimeString)), end=datetimeString)
+        time.sleep(0.001)
