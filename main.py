5b18b6f5cab291e98e6b63abb04a1d51e4926168
JrOrOneEquals1 <cdr5of8@gmail.com>
2021-11-17 15:04:10 -0700

    greatly shortened, and always does loop

--- main.py
+++ main.py
@@ -1,34 +1,13 @@
 import datetime
 import time
+
 utc = datetime.timezone.utc
 startDate = datetime.datetime(2021, 11, 17, 18, 59, 0, tzinfo=utc)
-timePassed = int((datetime.datetime.now().astimezone(utc) - startDate).total_seconds())
-datetimeList = [0, 0, 0, 0, 0, 0]
-timesDone = 0
-while timePassed >= 1:
-    datetimeList[timesDone] = int(str(timePassed)[-2:])
-    try:
-        timePassed = int(str(timePassed)[:-2])
-    except ValueError:
-        break
-    timesDone += 1
-datetimeString = f"{datetimeList[5]}/{datetimeList[4]}/{datetimeList[3]}   {datetimeList[2]}:{datetimeList[1]}:{datetimeList[0]}"
-print(f"{str(datetime.datetime.now()).split('.')[0]} in Galactic Time is:")
-print(datetimeString)
-print('\n')
+print('Galactic Time is currently:')
 
-if input('Loop? [y/n]\n>^< ').startswith('y'):
-    while True:
-        timePassed = str((datetime.datetime.now().astimezone(utc) - startDate).total_seconds())
-        datetimeList = [0, 0, 0, 0, 0, 0]
-        timesDone = 0
-        while int(timePassed) >= 1:
-            datetimeList[timesDone] = timePassed[-2:]
-            try:
-                timePassed = timePassed[:-2]
-            except ValueError:
-                break
-            timesDone += 1
-        datetimeString = f"{datetimeList[5]}/{datetimeList[4]}/{datetimeList[3]}   {datetimeList[2]}:{datetimeList[1]}:{datetimeList[0]}"
-        print(' '*20 + '\b'*(len(list(datetimeString)) + 30), end=datetimeString + '\033[?25l')
-        time.sleep(0.001)
+while True:
+    timePassed = str(int((datetime.datetime.now().astimezone(utc) - startDate).total_seconds()))
+    GTstring = '0'*(14 - len(timePassed)) + timePassed
+    datetimeString = f"{GTstring[0:4]}/{GTstring[4:6]}/{GTstring[6:8]}   {GTstring[8:10]}:{GTstring[10:12]}:{GTstring[12:]}"
+    print(' '*20 + '\b'*(len(list(datetimeString)) + 30), end=datetimeString + '\033[?25l')
+    time.sleep(0.001)
